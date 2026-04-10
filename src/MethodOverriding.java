f114e5773ec535f0711f9151cffe350b719d3547
vaishnav <vaishnssavkunschuaa@gmail.com>
2026-04-10 18:26:21 +0530

    MethodOverriding

--- src/MethodOverriding.java
+++ src/MethodOverriding.java
@@ -1,16 +1,25 @@
 class Employee {
-   
+    void display() {
+        System.out.println("Name of class is Employee");
+    }
 }
 
 class Engineer extends Employee {
-    // TODO: Override display() method
-    // 1. Print "Name of class is Engineer"
-    // 2. Call super.display() to print the parent's message
+    @Override
+    void display() {
+        // First print the Engineer's message
+        System.out.println("Name of class is Engineer");
+        // Then use the super keyword to call the base class's display method
+        super.display();
+    }
 }
 
 public class MethodOverriding {
-   
-        // TODO: Create an Engineer object
-        // TODO: Call the display() method
-    
+    public static void main(String[] args) {
+        // 1. Create an Engineer object.
+        Engineer myEngineer = new Engineer();
+        
+        // 2 & 3. Call display() on it (which will also call super.display() internally)
+        myEngineer.display();
+    }
 }
